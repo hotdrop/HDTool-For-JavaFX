58d1ed8075a189dbae83c9798f7e48fdcc65e170
kenji <gunsecondstage0106@gmail.com>
2016-03-06 00:30:35 +0900

    add disable condition of executeButton.

--- src/application/SSTController.java
+++ src/application/SSTController.java
@@ -5,6 +5,8 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -26,26 +28,33 @@ public class SSTController implements Initializable {
 	private TextField filePathField;
 	@FXML
 	private TextField searchWordField;
+	
+	@FXML
+	private Button buttonExecute;
+	
 	@FXML
 	private TableView<TableViewData> resultTableView;
 	@FXML
-	private TableColumn dateViewId;
+	private TableColumn<TableViewData, String> dateViewId;
 	@FXML
-	private TableColumn numViewId;
+	private TableColumn<TableViewData, Integer> numViewId;
 	@FXML
-	private TableColumn exceptionViewId;
+	private TableColumn<TableViewData, String> exceptionViewId;
 	@FXML
-	private TableColumn messageViewId;
+	private TableColumn<TableViewData, String> messageViewId;
 	@FXML
-	private TableColumn locationViewId;
+	private TableColumn<TableViewData, String> locationViewId;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		dateViewId.setCellValueFactory(new PropertyValueFactory<TableViewData, String>("date"));
-		numViewId.setCellValueFactory(new PropertyValueFactory<TableViewData, Integer>("number"));
-		exceptionViewId.setCellValueFactory(new PropertyValueFactory<TableViewData, String>("exception"));
-		messageViewId.setCellValueFactory(new PropertyValueFactory<TableViewData, String>("message"));
-		locationViewId.setCellValueFactory(new PropertyValueFactory<TableViewData, String>("location"));
+		dateViewId.setCellValueFactory(new PropertyValueFactory<>("date"));
+		numViewId.setCellValueFactory(new PropertyValueFactory<>("number"));
+		exceptionViewId.setCellValueFactory(new PropertyValueFactory<>("exception"));
+		messageViewId.setCellValueFactory(new PropertyValueFactory<>("message"));
+		locationViewId.setCellValueFactory(new PropertyValueFactory<>("location"));
+		
+		BooleanBinding bb = filePathField.textProperty().isEmpty().or(searchWordField.textProperty().isEmpty()); 
+		buttonExecute.disableProperty().bind(bb);
 	}
 	
 	@FXML
@@ -61,7 +70,7 @@ public class SSTController implements Initializable {
 	
 	@FXML
 	protected void onExecute(ActionEvent event) {
-		// TODO input check filePathField and searchWordField
+		
 		StackTraceFile stFile = new StackTraceFile(filePathField.getText(), searchWordField.getText());
 		stFile.read();
 		HashMap<String, StackTraceData> resultMap = stFile.getResultMap();
